e44d49084307cf16760d045d03b8143e383c3041
danielmoralesp <daniel_moralesp@hotmail.com>
2017-12-04 18:54:59 -0500

    Controller and Concerns for Block and Pin

---
+++ app/controllers/api/v1/block_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::BlockController < ApplicationController
+  include FindBlock
+
+  respond_to :json
+
+  def show
+    @block = Block.find(params[:id])
+    if @block
+      render :json => @block, serializer: BlockSerializer, root: "ip_block"
+    else
+      @error = Error.new(:text => "404 Not found", :status => 404, :url => request.url, :method => request.method)
+      render :json => @error.serializer
+    end
+  end
+
+  def get_block
+    if @block
+      render :json => @block, serializer: BlockSerializer, root: "ip_block"
+    else
+      @error = Error.new(:text => "404 Not found", :status => 404, :url => request.url, :method => request.method)
+      render :json => @error.serializer
+    end
+  end
+end
---
+++ app/controllers/api/v1/pin_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::PinController < ApplicationController
+  include EntryFiltering
+
+  respond_to :json
+
+  def index
+    if @pins
+      render :json => @pins, each_serializer: PinSerializer, root: "pins"
+    else
+      @error = Error.new(:text => "404 Not found", :status => 404, :url => request.url, :method => request.method)
+      render :json => @error.serializer
+    end
+  end
+
+  def show
+    @pin = Pin.find(params[:id])
+    if @pin
+      render :json => @pin, serializer: PinSerializer, root: "pin"
+    else
+      @error = Error.new(:text => "404 Not found", :status => 404, :url => request.url, :method => request.method)
+      render :json => @error.serializer
+    end
+  end
+end
