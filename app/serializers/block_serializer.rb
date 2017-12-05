ee32776da2d2cb261777c2a246660001d4bf379a
danielmoralesp <daniel_moralesp@hotmail.com>
2017-12-04 19:00:42 -0500

    Block and Pin serializers

---
+++ app/serializers/block_serializer.rb
@@ -0,0 +1,16 @@
+class BlockSerializer < ActiveModel::Serializer
+  attributes :network_start_ip, :geoname_id, :_links, :point, :latitude, :longitude
+
+  def _links
+    {:self => _self}
+  end
+
+  def _self
+    href = URI::encode("/api/v1/blocks/#{object.id}")
+    {:href => href, :method => "GET", :rel => "self"}
+  end
+
+  def point
+    object.lonlat
+  end
+end
---
+++ app/serializers/pin_serializer.rb
@@ -0,0 +1,21 @@
+class PinSerializer < ActiveModel::Serializer
+  attributes :title, :_links, :point, :longitude, :latitude
+
+  def _links
+    {:self => _self, :wiki => wiki_url}
+  end
+
+  def wiki_url
+    href = URI::encode(object.url)
+    {:href => href, :method => "GET", :rel => "wiki"}
+  end
+
+  def _self
+    href = URI::encode("/api/v1/pins/#{object.id}")
+    {:href => href, :method => "GET", :rel => "self"}
+  end
+
+  def point
+    {:latitude => object.lonlat.latitude, :longitude => object.lonlat.longitude}
+  end
+end
